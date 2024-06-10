fe31da7a9fa4b8e4dfd8c50b98eef13d3105cdc3
eriksgda <erik.ksgda@gmail.com>
2024-06-10 12:22:22 -0300

    add encapsulamento e métodos get

--- src/Model/Financing.java
+++ src/Model/Financing.java
@@ -1,7 +1,21 @@
+package Model;
+
 public class Financing {
-    double propertyValue;
-    int financingTerm;
-    double annualInterestRate;
+    private double propertyValue;   // valor do imóvel
+    private int financingTerm;  // prazo do financiamento
+    private double annualInterestRate;  // taxa de juros anual
+
+    public double getPropertyValue(){
+        return this.propertyValue;
+    }
+
+    public int getFinancingTerm(){
+        return this.financingTerm;
+    }
+
+    public double getAnnualInterestRate(){
+        return this.annualInterestRate;
+    }
 
     public Financing(double propertyValue, int financingTerm, double annualInterestRate){
         this.propertyValue = propertyValue;
@@ -15,5 +29,13 @@ public class Financing {
 
     public double calcTotalPayment(){
         return this.calcMonthlyPayment() * financingTerm * 12;
+    }
+
+    public void showFinancingData(){
+        System.out.println("Property value: " + this.propertyValue);
+        System.out.println("Financing term: " + this.financingTerm);
+        System.out.println("Annual interest rate: " + this.annualInterestRate);
+        System.out.println("Monthly payment of financing: " + calcMonthlyPayment());
+        System.out.println("Total payment of financing: " + calcTotalPayment());
     }
 }
