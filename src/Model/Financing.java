e54c39d0669b3bbb3b5d6f642ae7139f15f07411
eriksgda <erik.ksgda@gmail.com>
2024-06-21 20:56:19 -0300

    uptade to abstract class

--- src/Model/Financing.java
+++ src/Model/Financing.java
@@ -1,18 +1,18 @@
 package Model;
 
-import java.text.NumberFormat;
-
-public class Financing {
+public abstract class Financing {
     protected double propertyValue;   // valor do imóvel
     protected int financingTerm;  // prazo do financiamento
     protected double annualInterestRate;  // taxa de juros anual
 
+    // constructor
     public Financing(double propertyValue, int financingTerm, double annualInterestRate){
         this.propertyValue = propertyValue;
         this.financingTerm = financingTerm;
         this.annualInterestRate = annualInterestRate;
     }
 
+    // getters
     public double getPropertyValue(){
         return this.propertyValue;
     }
@@ -25,19 +25,12 @@ public class Financing {
         return this.annualInterestRate;
     }
 
+    // other methods
     public double calcMonthlyPayment(){
         return (getPropertyValue() / (getFinancingTerm() * 12)) * (1 + (getAnnualInterestRate() / 100) / 12);
     }
 
-    public double calcTotalPayment(){
-        return calcMonthlyPayment() * getFinancingTerm() * 12;
-    }
+    public abstract double calcTotalPayment();
 
-    public void printFinancingData(){
-        System.out.println("Property value: R$ " + NumberFormat.getCurrencyInstance().format(getPropertyValue()));
-        System.out.println("Financing term: " + this.financingTerm  + " year(s)");
-        System.out.println("Annual interest rate: " +  this.annualInterestRate + '%'); //NumberFormat.getPercentInstance().format(this.annualInterestRate)
-        System.out.println("Monthly payment of financing: R$ " + NumberFormat.getCurrencyInstance().format(calcMonthlyPayment()));
-        System.out.println("Total payment of financing: R$ " + NumberFormat.getCurrencyInstance().format(calcTotalPayment()));
-    }
+    public abstract void printFinancingData();
 }
