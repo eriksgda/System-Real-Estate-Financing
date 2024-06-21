3971eb96655fc572bebbe4999e871a3835ee659f
eriksgda <erik.ksgda@gmail.com>
2024-06-21 20:54:48 -0300

    add new attributes and add abstract methods

--- src/Model/House.java
+++ src/Model/House.java
@@ -1,14 +1,46 @@
 package Model;
 
+import java.text.NumberFormat;
+
 public class House extends Financing {
+    // attributes
+    private double houseArea;
+    private double landArea;
 
-
-    public House(double propertyValue, int financingTerm, double annualInterestRate){
+    // constructor
+    public House(double propertyValue, int financingTerm, double annualInterestRate, double houseArea, double landArea){
         super(propertyValue, financingTerm, annualInterestRate);
+        this.houseArea = houseArea;
+        this.landArea = landArea;
     }
 
+    // getters
+    public double getHouseArea() {
+        return this.houseArea;
+    }
+
+    public double getLandArea() {
+        return this.landArea;
+    }
+
+    // other methods
     @Override
     public double calcMonthlyPayment(){
         return super.calcMonthlyPayment() + 80;
+    }
+
+    public double calcTotalPayment(){
+        return calcMonthlyPayment() * getFinancingTerm() * 12;
+    }
+
+    public void printFinancingData(){
+        System.out.println("Type  of financing: House");
+        System.out.println("House area: " + getHouseArea() + "m²");
+        System.out.println("Number of garage space: " + getLandArea() + "m²");
+        System.out.println("Property value: R$ " + NumberFormat.getCurrencyInstance().format(getPropertyValue()));
+        System.out.println("Financing term: " + this.financingTerm  + " year(s)");
+        System.out.println("Annual interest rate: " +  this.annualInterestRate + '%'); //NumberFormat.getPercentInstance().format(this.annualInterestRate)
+        System.out.println("Monthly payment of financing: R$ " + NumberFormat.getCurrencyInstance().format(calcMonthlyPayment()));
+        System.out.println("Total payment of financing: R$ " + NumberFormat.getCurrencyInstance().format(calcTotalPayment()));
     }
 }
