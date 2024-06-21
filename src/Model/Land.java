c5952843039e5c874f787cc81ce5d61958a46829
eriksgda <erik.ksgda@gmail.com>
2024-06-21 20:55:22 -0300

    add new attributes and add abstract methods

--- src/Model/Land.java
+++ src/Model/Land.java
@@ -1,13 +1,39 @@
 package Model;
 
-public class Land extends Financing{
+import java.text.NumberFormat;
 
-    public Land(double propertyValue, int financingTerm, double annualInterestRate){
+public class Land extends Financing{
+    // attributes
+    private String areaType;
+
+    // constructor
+    public Land(double propertyValue, int financingTerm, double annualInterestRate, String areaType){
         super(propertyValue, financingTerm, annualInterestRate);
+        this.areaType = areaType;
     }
 
+    // getters
+    public String getAreaType() {
+        return areaType;
+    }
+
+    // other methods
     @Override
     public double calcMonthlyPayment(){
         return super.calcMonthlyPayment() * 1.02;
+    }
+
+    public double calcTotalPayment(){
+        return calcMonthlyPayment() * getFinancingTerm() * 12;
+    }
+
+    public void printFinancingData(){
+        System.out.println("Type  of financing: Land");
+        System.out.println("Area type: " + getAreaType());
+        System.out.println("Property value: R$ " + NumberFormat.getCurrencyInstance().format(getPropertyValue()));
+        System.out.println("Financing term: " + this.financingTerm  + " year(s)");
+        System.out.println("Annual interest rate: " +  this.annualInterestRate + '%'); //NumberFormat.getPercentInstance().format(this.annualInterestRate)
+        System.out.println("Monthly payment of financing: R$ " + NumberFormat.getCurrencyInstance().format(calcMonthlyPayment()));
+        System.out.println("Total payment of financing: R$ " + NumberFormat.getCurrencyInstance().format(calcTotalPayment()));
     }
 }
