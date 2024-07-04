fede8fce06c6a4edb587e20ba2962253dbf99861
eriksgda <erik.ksgda@gmail.com>
2024-07-04 19:54:40 -0300

    update exception use

--- src/Util/InterfaceUser.java
+++ src/Util/InterfaceUser.java
@@ -8,15 +8,19 @@ import Exceptions.InputNegativeValueException;
 public class InterfaceUser {
     Scanner scanner = new Scanner(System.in);
 
+    private void validation(double value, String errorMessage) throws InputNegativeValueException{
+        if (value <= 0) {
+            throw new InputNegativeValueException(errorMessage);
+        }
+    }
+
     public double inputPropertyValue(){
         double value = 0;
         do{
             System.out.println("Enter the Property Value: ");
             try{
                 value = scanner.nextDouble();
-                if (value <= 0) {
-                    throw new InputNegativeValueException("Please enter a positive value for the Property Value.");
-                }
+                validation(value, "Please enter a positive value for the Property Value.");
             }catch (InputMismatchException exception){
                 System.out.println("Invalid input. Please enter a numeric value for the Property Value.");
                 scanner.next();
@@ -34,9 +38,7 @@ public class InterfaceUser {
             System.out.println("Enter the Financing Term: ");
             try {
                 term = scanner.nextInt();
-                if (term <= 0){
-                    throw new InputNegativeValueException("Please enter a positive value for the Financing Term.");
-                }
+                validation(term, "Please enter a positive value for the Financing Term.");
             } catch (InputMismatchException exception){
                 System.out.println("Invalid input. Please enter an integer value for the Financing Term.");
                 scanner.next();
@@ -54,9 +56,7 @@ public class InterfaceUser {
             System.out.println("Enter the Annual Interest Rate: ");
             try {
                 rate = scanner.nextDouble();
-                if (rate <= 0){
-                    throw new InputNegativeValueException("Please enter a positive value for the Annual Interest Rate.");
-                }
+                validation(rate, "Please enter a positive value for the Annual Interest Rate.");
             } catch (InputMismatchException exception){
                 System.out.println("Invalid input. Please enter a numeric value for the Annual Interest Rate.");
                 scanner.next();
