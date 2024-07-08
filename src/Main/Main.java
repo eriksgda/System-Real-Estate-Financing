29ed3709f015f4f381b86e7d6ad667d87460dfee
eriksgda <erik.ksgda@gmail.com>
2024-07-08 16:53:16 -0300

    create file/reader .txt && create comments && create test of .txt and .test

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,9 +7,15 @@ import java.util.ArrayList;
 import Model.*;
 import Util.*;
 
+// Professor, estava tentando fazer com o objectInputStream e o append = true, porém dava erro na leitura, a forma que encontrei para
+// arrumar me pareceu longo demais (https://www.geeksforgeeks.org/how-to-fix-java-io-streamcorruptedexception-invalid-type-code-in-java/),
+// então, optei por fazer sem o append = true, passando os dados do arquivo pra uma arraylist e depois sobrescrevendo por cima,
+// ou seja, peguei os dados adicionei os novos e escrevi todos desde o começo no mesmo arquivo. Acredito que não é a melhor forma, já
+// que quando se inicializa o programa, caso não exista o arquivo, vai dar o erro de FileNotFoundException, ainda assim, acredito
+// que cumpra todos os requisitos pedidos no projeto.
 
 public class Main {
-    // write method
+    // write method (ObjectOutputStream)
     public static void writeFinancing(String fileName, ArrayList<Financing> financingList){
         ObjectOutputStream outputStream;
         try {
@@ -25,7 +31,7 @@ public class Main {
             System.out.println(exception.getMessage());
         }
     }
-    // read method
+    // read method (ObjectInputStream)
     public static ArrayList<Financing> readFinancing(String fileName) {
         ObjectInputStream inputStream;
         ArrayList<Financing> financingList = new ArrayList<>();
@@ -50,6 +56,40 @@ public class Main {
         return financingList;
     }
 
+    // write method (FileWriter)
+    public static void writeFile(String fileName, Financing financing){
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter(fileName, true);
+
+            fileWriter.write(financing.toString());
+
+            fileWriter.flush();
+            fileWriter.close();
+        }catch (IOException exception){
+            System.out.println();
+        }
+    }
+
+    // read method (FileReader)
+    public static void readFile(String fileName){
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(fileName);
+            int counter;
+            while((counter = fileReader.read()) != -1){
+                System.out.print((char)counter);
+            }
+
+            fileReader.close();
+        } catch (FileNotFoundException exception){
+            System.out.println();
+        } catch (IOException exception){
+            System.out.println(exception.getMessage());
+        }
+
+    }
+
     public static void createSomeFinancing(ArrayList<Financing> financingList){
         Financing secondApartmentFinancing = new Apartment(1000000,15,
                 10, 50, 13);
@@ -66,14 +106,19 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the financing simulator!");
+        System.out.println("Welcome to the Financing Simulator!");
 
+        //variables
         String fileName = "src/financing.test";
+        String fileNameTxt = "src/financing.txt";
+        double totalPropertyValue = 0;
+        double totalFinancingValue = 0;
         double propertyValue;
         int financingTerm;
         double annualInterestRate;
 
-        ArrayList<Financing> financingList = readFinancing(fileName);
+        ArrayList<Financing> financingList = readFinancing(fileName); // Arraylist for .test
+        ArrayList<Financing> financingListTxt = new ArrayList<>(); // ArrayList for .txt
 
         InterfaceUser interfaceUser = new InterfaceUser();
 
@@ -88,6 +133,8 @@ public class Main {
 
                 Financing apartmentFinancing = new Apartment(propertyValue, financingTerm, annualInterestRate, garageSpace, floorNumber);
                 financingList.add(apartmentFinancing);
+                financingListTxt.add(apartmentFinancing);
+
                 break;
             case 2: // house
                 propertyValue = interfaceUser.inputPropertyValue();
@@ -98,6 +145,7 @@ public class Main {
 
                 Financing houseFinancing = new House(propertyValue, financingTerm, annualInterestRate, houseArea, landArea);
                 financingList.add(houseFinancing);
+                financingListTxt.add(houseFinancing);
                 break;
             case 3: // land
                 propertyValue = interfaceUser.inputPropertyValue();
@@ -107,16 +155,19 @@ public class Main {
 
                 Financing landFinancing = new Land(propertyValue, financingTerm, annualInterestRate, (areaType ? "Residential" : "Commercial"));
                 financingList.add(landFinancing);
+                financingListTxt.add(landFinancing);
                 break;
         }
 
         createSomeFinancing(financingList);
+        createSomeFinancing(financingListTxt);
 
+        // Object Input/Output Stream
         writeFinancing(fileName, financingList);
         ArrayList<Financing> financingListData = readFinancing(fileName);
 
-        double totalPropertyValue = 0;
-        double totalFinancingValue = 0;
+        System.out.println("-> Print with Object Input Stream (.test):\n");
+
         for (Financing financing : financingListData){
             financing.printFinancingData();
             System.out.println();
@@ -125,7 +176,16 @@ public class Main {
             totalFinancingValue += financing.calcTotalPayment();
         }
 
-        System.out.printf("\nTotal property value: R$ %s Total Financing Value: R$ %s",
+        // File Writer/Reader
+        System.out.println("-> Print with File Reader (.txt): \n");
+
+        for (Financing financing : financingListTxt){
+            writeFile(fileNameTxt, financing);
+        }
+
+        readFile(fileNameTxt);
+
+        System.out.printf("\nTotal property value: %s Total Financing Value: %s",
                 NumberFormat.getCurrencyInstance().format(totalPropertyValue),
                 NumberFormat.getCurrencyInstance().format(totalFinancingValue));
 
